5f1928e235585cc16cc3e59b6cbfa5e6cf01eef5
Taylor Phillips <taylorphillips133@gmail.com>
2021-01-20 12:32:28 -0800

    code is successful to post to db  using postman

--- api/models.py
+++ api/models.py
@@ -1,5 +1,4 @@
 from . import db
-
 class Movie(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(50))
--- api/views.py
+++ api/views.py
@@ -1,10 +1,17 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
+from . import db
+from .models import Movie
 
 main = Blueprint('main', __name__)
 
-
 @main.route('/add_movie', methods=['POST'])
 def add_movie():
+    movie_data = request.get_json()
+
+    new_movie = Movie(title=movie_data['title'], rating=movie_data['rating'])
+
+    db.session.add(new_movie)
+    db.session.commit()
 
     return 'Done', 201
 
