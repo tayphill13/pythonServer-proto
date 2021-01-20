24bdbdfca575a88b5cb54b7b2d643689ac167e5c
Taylor Phillips <taylorphillips133@gmail.com>
2021-01-20 12:09:01 -0800

    begin python server with flask and first time python with vscode '

---
+++ api/views.py
@@ -0,0 +1,16 @@
+from flask import Blueprint, jsonify
+
+main = Blueprint('main', __name__)
+
+
+@main.route('/add_movie', methods=['POST'])
+def add_movie():
+
+    return 'Done', 201
+
+@main.route('/movies')
+def movies():
+
+    movies = []
+
+    return jsonify({'movies' : movies})
